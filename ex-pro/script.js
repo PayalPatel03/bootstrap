e2400404af32c08f4a62de9532625956614ba117
unknown <p6579113@gmail.com>
2024-06-19 21:48:16 +0530

    nav

---
+++ ex-pro/script.js
@@ -0,0 +1,18 @@
+// ----------- T O G G L E - T H E M E ----------------
+
+$(document).ready(function () {
+    $(".fa-sun").hide();
+    $(".fa-moon").hide();
+  
+    const systemSettingDark = window.matchMedia("(prefers-color-scheme: light)");
+    if (systemSettingDark.matches) {
+      $("body").addClass("light-mode");
+      $(".fa-sun").show();
+    } else {
+      $("body").addClass("dark-mode");
+      $(".fa-moon").show();
+    }
+    $(".slider").on("click", function () {
+      $("body").toggleClass("dark-mode");
+    });
+  });
